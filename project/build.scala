caceab3a364130a64055e5cf3f8f398050a4ece8
xuwei-k <6b656e6a69@gmail.com>
2012-04-27 02:21:32 +0900

    chenge maven => sbt

---
+++ project/build.scala
@@ -0,0 +1,35 @@
+import sbt._,Keys._
+
+object build extends Build{
+
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+    javacOptions ++= Seq("-Xlint")
+  )
+
+  lazy val root = Project(
+    "root",
+    file(".")
+  )aggregate(core,sample)
+
+  lazy val core = Project(
+    "core",
+    file("core"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies ++= Seq(
+        "org.slf4j" % "slf4j-api" % "1.6.4"
+      )
+    )
+  )
+
+  lazy val sample = Project(
+    "sample",
+    file("sample"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies ++= Seq(
+        "junit" % "junit" % "3.8.1" % "test"
+       ,"com.novocode" % "junit-interface" % "0.8" % "test->default"
+      )
+    )
+  ) dependsOn(core)
+}
+
